2117edc19be6495ce6da334b36bec10f553c275f
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-08-16 22:06:17 +0300

    9.3. Преобразование примитивных типов [#383106]

---
+++ src/main/java/ru/job4j/condition/Types.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class Types {
+    public static void main(String[] args) {
+        long l = 129;
+        byte b = (byte) l;
+        System.out.println(b);
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println(rsl);
+        char c = 45000;
+        float g = c;
+        System.out.println(g);
+        double d = 121.19;
+        byte fu = (byte) d;
+        System.out.println(fu);
+        short s = 1500;
+        char k = (char) s;
+        double u = k;
+        System.out.println(g);
+    }
+}
