77fed675989469ac9934ac16c406bdf25b051967
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-08-17 19:37:42 +0300

    9. Площадь треугольника. [#175474 #333099]

--- src/main/java/ru/job4j/calculator/TrgArea.java
+++ src/main/java/ru/job4j/calculator/TrgArea.java
@@ -2,15 +2,10 @@ package ru.job4j.calculator;
 
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double one = a + b + c;
-        double two = one / 2;
-        double three = two - a;
-        double four = two - b;
-        double five = two - c;
-        double six = three * four * five;
-        double seven = two * six;
-        double eight = Math.sqrt(seven);
-        return eight;
+        double one = (a + b + c) / 2;
+        double two = one * (one - a) * (one - b) * (one - c);
+        double tree = Math.sqrt(two);
+        return tree;
     }
 
     public static void main(String[] args) {
