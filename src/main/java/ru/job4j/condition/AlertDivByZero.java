92c9afb84ff5efa3f8e22b78bd67302f1aa639f8
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-08-23 06:45:59 +0300

    4.1. Операторы ветвлений [#3611 #333915]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -11,8 +11,24 @@ public class AlertDivByZero {
         if (number == 0) {
             System.out.println("Could not div by 0.");
         }
-            if (number < 0) {
-                System.out.println("This is negative number.");
-            }
+        if (number < 0) {
+            System.out.println("This is negative number.");
+        }
+    }
+
+    public static class SqArea {
+        public static double square(int p, double k) {
+            double h = p / (2 * (k + 1));
+            double l = h * k;
+            double s = l * h;
+            return s;
+        }
+
+        public static void main(String[] args) {
+            double result1 = SqArea.square(4, 1);
+            double result2 = SqArea.square(6, 2);
+            System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+            System.out.println(" p = 6, k = 2, s = 2, real = " + result2);
+        }
     }
 }
