0f12e21051b7783274261920d201d28288eef7e5
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-08-31 18:36:31 +0300

    4.5. Отладка программы в IDEA [#159464 #338023]

--- src/test/java/ru/job4j/condition/ThreeMaxTest.java
+++ src/test/java/ru/job4j/condition/ThreeMaxTest.java
@@ -7,10 +7,10 @@ public class ThreeMaxTest {
     @Test
     public void firstMax() {
         int first = 10;
-        int second = 50;
+        int second = 5;
         int third = 1;
         int result = ThreeMax.max(first, second, third);
-        int expected = 1;
+        int expected = 10;
         assertThat(result).isEqualTo(expected);
     }
 
@@ -20,7 +20,7 @@ public class ThreeMaxTest {
         int second = 150;
         int third = 1;
         int result = ThreeMax.max(first, second, third);
-        int expected = 1;
+        int expected = 150;
         assertThat(result).isEqualTo(expected);
     }
 
@@ -28,6 +28,16 @@ public class ThreeMaxTest {
     public void thirdMax() {
         int first = 2;
         int second = 5;
+        int third = 100;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 100;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void allEq() {
+        int first = 1;
+        int second = 1;
         int third = 1;
         int result = ThreeMax.max(first, second, third);
         int expected = 1;
@@ -35,32 +45,22 @@ public class ThreeMaxTest {
     }
 
     @Test
-    public void allEq() {
-        int first = 12;
-        int second = 19;
-        int third = 11;
-        int result = ThreeMax.max(first, second, third);
-        int expected = 11;
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
     public void firstEqSecond() {
         int first = 8;
-        int second = 10;
+        int second = 8;
         int third = 5;
         int result = ThreeMax.max(first, second, third);
-        int expected = 5;
+        int expected = 8;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void firstEqThird() {
         int first = 55;
-        int second = 66;
-        int third = 44;
+        int second = 44;
+        int third = 3;
         int result = ThreeMax.max(first, second, third);
-        int expected = 44;
+        int expected = 55;
         assertThat(result).isEqualTo(expected);
     }
 
@@ -68,9 +68,9 @@ public class ThreeMaxTest {
     public void secondEqThird() {
         int first = 1;
         int second = 100;
-        int third = 10;
+        int third = 100;
         int result = ThreeMax.max(first, second, third);
-        int expected = 10;
+        int expected = 100;
         assertThat(result).isEqualTo(expected);
     }
 }
